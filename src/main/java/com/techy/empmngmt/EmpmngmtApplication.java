e4e8ede4663e4db073d9ad58b7d5f196939e6758
mamidi-santhosh <m.santhosh911@gmail.com>
2018-12-02 22:18:08 +0530

    first commit

---
+++ src/main/java/com/techy/empmngmt/EmpmngmtApplication.java
@@ -0,0 +1,12 @@
+package com.techy.empmngmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpmngmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpmngmtApplication.class, args);
+	}
+}
---
+++ src/main/java/com/techy/empmngmt/model/Employee.java
@@ -0,0 +1,44 @@
+package com.techy.empmngmt.model;
+
+public class Employee {
+
+    private  int empId;
+
+    private String empName;
+
+    private String empNo;
+
+    private String empMailId;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getEmpNo() {
+        return empNo;
+    }
+
+    public void setEmpNo(String empNo) {
+        this.empNo = empNo;
+    }
+
+    public String getEmpMailId() {
+        return empMailId;
+    }
+
+    public void setEmpMailId(String empMailId) {
+        this.empMailId = empMailId;
+    }
+}
